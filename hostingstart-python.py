d92a60ff5fc0b0a9bc3a935e12c65a4f81dac53b
pawansingre <pawan.singre@gmail.com>
2021-06-09 11:48:00 +0530

    Update hostingstart-python.py

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -1,7 +1,9 @@
 
-from flask import Flask, render_template, request
+import sys
+import platform
 
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return render_template("hostingstart-python.html")
+def application(environ, start_response):
+    start_response(b'200 OK', [(b'Content-Type', b'text/html')])
+    with open ("hostingstart-python.html", "r") as hostingstart_file:
+        hosting = hostingstart_file.read()
+        yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
