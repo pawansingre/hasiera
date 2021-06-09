3c67ea71f4f55dab20c3113a410fb6475c0a9539
pawansingre <pawan.singre@gmail.com>
2021-06-09 09:24:09 +0530

    Update hostingstart-python.py

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -1,9 +1,5 @@
 from flask import Flask, jsonify,render_template, request
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
-
 @app.route("/")
 def hello():
     return render_template('hostingstart-python.html')
